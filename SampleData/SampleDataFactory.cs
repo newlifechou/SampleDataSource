ad8ce43ba24cbd153df05346ec3e4f05a2f29c6b
xs.zhou <newlifechou@outlook.com>
2016-04-07 14:02:46 +0800

    create a new project

---
+++ SampleData/SampleDataFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleData
+{
+    /// <summary>
+    /// 样板数据工厂
+    /// </summary>
+    /// <typeparam name="T">代表任意实现了接口的类</typeparam>
+    public class SampleDataFactory<T> where T: class
+    {
+
+    }
+}
